edc407e7f8d0246e77d447f2f59237867f0a5eb5
kristianwahlroos <kristian.whl@gmail.com>
2019-12-28 22:30:22 +0200

    8_1

--- a8/src/main.rs
+++ a8/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+use std::iter::Iterator;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf_reader = BufReader::new(File::open("input.txt").unwrap());
+    let mut chunks: Vec<Vec<u8>> = Vec::new();
+    loop {
+        let mut buffer = [0u8; 150];
+        match buf_reader.read_exact(&mut buffer) {
+            Ok(_) => (),
+            Err(_) => break,
+        };
+        chunks.push(buffer.to_vec());
+    }
+    let mut answer = (std::usize::MAX, 0);
+    for chunk in &chunks {
+        let zero_count = chunk.iter().filter(|&x| *x == b'0').count();
+        if answer.0 > zero_count {
+            answer.0 = zero_count;
+            let one_count = chunk.iter().filter(|&x| *x == b'1').count();
+            let two_count = chunk.iter().filter(|&x| *x == b'2').count();
+            answer.1 = one_count * two_count;
+        }
+    }
+    println!("Part 1 answer: {}", answer.1);
 }
